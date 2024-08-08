09a94e998b96ffa3232926b750da4b1a2c1d7010
NotCoolMan <138533348+notcoolg@users.noreply.github.com>
2024-08-08 17:59:46 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,1 @@
-async function fetchImages() {
-  try {
-    const response = await fetch('https://api.github.com/repos/notcoolg/image-repo/contents/images');
-    const files = await response.json();
-    const imageFiles = files.filter(file => file.type === 'file');
-    const randomFile = imageFiles[Math.floor(Math.random() * imageFiles.length)];
-    const imageUrl = randomFile.download_url;
 
-    document.getElementById('image').src = imageUrl;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-  }
-}
-
-window.onload = fetchImages;
