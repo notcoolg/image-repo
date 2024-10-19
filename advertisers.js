69aca7f3a90420109386ac92aa0191f6c0968d0b
NotCoolMan <138533348+notcoolg@users.noreply.github.com>
2024-10-19 01:18:11 -0400

    Update advertisers.js
    Signed-off-by: NotCoolMan <138533348+notcoolg@users.noreply.github.com>

--- advertisers.js
+++ advertisers.js
@@ -1,14 +1,14 @@
 const advertisers = {
   'xxavvADS': {
     href: 'https://mxavniiv.wixstudio.io/xadsindex/advertiser-pages/xxavvads',
-    text: 'Visit xxavvADS'
+    text: '𝓲'
   },
   'xxavv6AMES': {
     href: 'https://mxavniiv.wixstudio.io/xadsindex/advertiser-pages/xxavv6ames',
-    text: 'Visit xxavv6AMES'
+    text: '𝓲'
   },
   'xxavvMusik': {
     href: 'https://mxavniiv.wixstudio.io/xadsindex/advertiser-pages/xxavvmusik',
-    text: 'Visit xxavvMusik'
+    text: '𝓲'
   }
 };
