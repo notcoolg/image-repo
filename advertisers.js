a36ea9425d5fd56e06bf5f3f067fc6ed46a51a7b
NotCoolMan <138533348+notcoolg@users.noreply.github.com>
2024-10-19 01:08:25 -0400

    Create advertisers.js
    Signed-off-by: NotCoolMan <138533348+notcoolg@users.noreply.github.com>

---
+++ advertisers.js
@@ -0,0 +1,14 @@
+const advertisers = {
+  'xAxA': {
+    href: 'https://ads.xxavvgroup.com',
+    text: 'Visit xxavvADS'
+  },
+  'xAx6': {
+    href: 'https://6ames.xxavvgroup.com',
+    text: 'Visit xxavv6AMES'
+  },
+  'xAxM': {
+    href: 'https://musik.xxavvgroup.com',
+    text: 'Visit xxavvMusik'
+  }
+};
