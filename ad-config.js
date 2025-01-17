01ca7c82575cbc784a3a9a9d19fa303e8bc066a8
NotCoolMan <138533348+notcoolg@users.noreply.github.com>
2025-01-16 20:32:17 -0500

    Create ad-config.js
    Signed-off-by: NotCoolMan <138533348+notcoolg@users.noreply.github.com>

---
+++ ad-config.js
@@ -0,0 +1,24 @@
+// ad-config.js
+const adConfig = {
+  ads: [
+    {
+      src: 'images/image1.png',
+      href: 'https://ads.xxavvgroup.com',
+      alt: 'xxavvADS',
+      infoText: 'Check out xxavvADS!'
+    },
+    {
+      src: 'images/image2.png',
+      href: 'https://6ames.xxavvgroup.com',
+      alt: 'xxavv6AMES',
+      infoText: 'Visit xxavv6AMES website!'
+    },
+    {
+      src: 'images/image3.gif',
+      href: 'https://musik.xxavvgroup.com',
+      alt: 'xxavvMusik',
+      infoText: 'Discover xxavvMusik!'
+    }
+  ],
+  rotationInterval: 10000 // 10 seconds
+};
